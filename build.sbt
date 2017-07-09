8589567d60c591c4c8c6b0517487b632f78d3a8f
klangner <klangner@gmail.com>
2017-07-09 15:39:29 +0200

    Added Akka HTTP

--- build.sbt
+++ build.sbt
@@ -3,4 +3,7 @@ name := "script-engine"
 version := "0.1.0"
 
 scalaVersion := "2.12.1"
-        
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-http" % "10.0.9"
+)
--- src/main/scala/com/carl/se/Main.scala
+++ src/main/scala/com/carl/se/Main.scala
@@ -1,11 +1,33 @@
 package com.carl.se
 
-/**
-  * Run server for serving REST API
-  */
-object Main {
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+import scala.io.StdIn
 
-  def main(args: Array[String]): Unit = {
-    println("server not implemented yet!")
+object Main {
+  def main(args: Array[String]) {
+
+    implicit val system = ActorSystem("script-engine")
+    implicit val materializer = ActorMaterializer()
+    // needed for the future flatMap/onComplete in the end
+    implicit val executionContext = system.dispatcher
+
+    val route =
+      path("hello") {
+        get {
+          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>\n"))
+        }
+      }
+
+    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+
+    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+    StdIn.readLine() // let it run until user presses return
+    bindingFuture
+      .flatMap(_.unbind()) // trigger unbinding from the port
+      .onComplete(_ => system.terminate()) // and shutdown when done
   }
 }
